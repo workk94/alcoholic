0d0fb35c3a3a4c6688981aadee881adb0c1030cb
김무궁 <rlaanrnd1027@naver.com>
2024-10-14 14:56:31 +0900

    asd

--- web/src/main/java/web/userpage/UpdateCartServlet.java
+++ web/src/main/java/web/userpage/UpdateCartServlet.java
@@ -15,43 +15,56 @@ public class UpdateCartServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
         // 현재 세션에서 장바구니 가져오기
-        HttpSession session = request.getSession();
-        ArrayList<Product> cartlist = (ArrayList<Product>) session.getAttribute("cartlist");
-        ProductDAO dao = new ProductDAO();
+    	  
+        int quantity=   Integer.parseInt( request.getParameter("quantity"));
+        String code= request.getParameter("code");
         
+      
         
-        if (cartlist != null) {
-            // 제품 수량 업데이트
-            for (Product product : cartlist) {
-                String quantityParam = request.getParameter("quantity_" + product.getProd_no());
-                
-                if (quantityParam != null) {
-                    try {
-                        int quantity = Integer.parseInt(quantityParam);
-                        // 수량이 0보다 작지 않도록 확인
-                        if (quantity > 0) {
-                        	  // 데이터베이스에 수량 업데이트
-                            dao.UpdateOrderItem(product.getProd_no(), quantity);
-                            product.setQuantity(quantity); // Product 클래스에 setQuantity 메서드가 있어야 함
-                        } else {
-                            // 수량이 0이면 장바구니에서 삭제
-                            cartlist.remove(product);
-                        }
-                    } catch (NumberFormatException e) {
-                        // 수량 파싱 오류 처리 (예: 숫자가 아닐 경우)
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
+        //세션 삭제, 장바구니 삭제
+    	 deleteSession( request , code);      	
+    	
+    	 ProductDAO dao = new ProductDAO();
+         dao.UpdateOrderItem(code, quantity);
 
         // 장바구니를 다시 세션에 저장
-        session.setAttribute("cartlist", cartlist);
+      
         
         
 
         // 장바구니 페이지로 리다이렉트
         response.sendRedirect(request.getContextPath() + "/order");
+    
+    }
+    
+    
+    
+    public void deleteSession(  HttpServletRequest   request , String code) {
+    	
+        HttpSession session = request.getSession();
+        ArrayList<Product> cartlist = (ArrayList<Product>) session.getAttribute("cartlist");
+      
+        
+        
+        Product  search=null;
+        if (cartlist != null) {
+            // 제품 수량 업데이트
+            for (Product product : cartlist) {
+                 
+                      if( product.getProd_no().equals(code))   {
+                    	  search =product;
+                    	   
+                   }
+            }            
+            
+            cartlist.remove(search); 
+            
+            System.out.println("삭제후 ==>") ;
+            
+            System.out.println( cartlist);
+            
+          
+        }
     
     }
 }
